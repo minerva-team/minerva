ba7e639aca4c095b9fdca47cf9211909d6417313
Mahtab Saberi <saberimahtab2002@gmail.com>
2026-06-21 18:00:47 -0700

    feat : add User model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,5 +1,24 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, unique=True, null=True, blank=True)
+    
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('HR Manager', 'HR Manager'),
+        ('Finance Manager', 'Finance Manager'),
+        ('Employee', 'Employee')
+    ]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    
+    def __str__(self):
+        return self.email
