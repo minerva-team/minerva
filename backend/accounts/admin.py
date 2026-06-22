39d031a738798b463e6a2d7966e994374ffc4458
Mahtab Saberi <saberimahtab2002@gmail.com>
2026-06-22 14:22:32 -0700

    feat : add User admin pannel

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, OTPVerification
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ['email', 'role', 'is_active', 'created_at']
+    search_fields = ['email', 'phone_number']
+    ordering = ['-created_at']
+    list_filter = ['role', 'is_active']
+    
