317e9aa544171213bb61389054fee015208ed16e
shahinzam101 <shahinzam1402@gmail.com>
2026-06-23 23:22:05 +0330

    feat: modify Jazzmin

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -43,6 +43,7 @@ ALLOWED_HOSTS = env_list("DJANGO_ALLOWED_HOSTS")
 # --- Applications -----------------------------------------------------------
 
 DJANGO_APPS = [
+    "jazzmin",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -150,4 +151,83 @@ SPECTACULAR_SETTINGS = {
     "DESCRIPTION": "HR & Finance ERP",
     "VERSION": "0.1.0",
     "SERVE_INCLUDE_SCHEMA": False,
+}
+
+JAZZMIN_SETTINGS = {
+    "site_title": "Minerva Admin",
+    "site_header": "Minerva System",
+    "site_brand": "Minerva",
+    "site_logo": None,  
+    "site_logo_classes": "img-circle",
+    "site_icon": None,
+    "welcome_sign": "System Administration Panel",
+    "copyright": "Minerva © 2026",
+    "search_model": ["accounts.User"],
+    "show_ui_builder": False,
+    
+    "topmenu_links": [
+        {"name": "Home", "url": "admin:index", "permissions": ["auth.view_user"]},
+        {"name": "Support", "url": "/support/", "new_window": True},
+    ],
+    
+    "usermenu_links": [
+        {"model": "auth.user"}
+    ],
+    
+    "icons": {
+        "accounts.user": "fas fa-users",
+        "accounts.otpverification": "fas fa-key",
+        "auth.Group": "fas fa-users-cog",
+    },
+    
+    "default_icon_parents": "fas fa-chevron-circle-right",
+    "default_icon_children": "fas fa-circle",
+    
+    "related_modal_active": False,
+    "custom_css": "css/custom_jazzmin.css",
+    "custom_js": None,
+    "use_google_fonts_cdn": True,
+    "show_sidebar": True,
+    "navigation_expanded": True,
+    "hide_apps": [],
+    "hide_models": [],
+    "order_with_respect_to": ["accounts"],
+    
+    "changeform_format": "horizontal_tabs",
+    "changeform_format_overrides": {
+        "auth.user": "collapsible",
+        "auth.group": "vertical_tabs",
+    },
+}
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "brand_colour": "#512988",
+    "accent": "accent-purple",
+    "navbar": "#512988",
+    "no_navbar_border": False,
+    "navbar_fixed": True,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": True,
+    "sidebar": "sidebar-dark-purple",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "darkly",
+    "dark_mode_theme": "darkly",
+    "button_classes": {
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    },
 }
