e855fb1ff308b042bd8c95d3c707b6fb400d880c
Mahtab Saberi <saberimahtab2002@gmail.com>
2026-06-23 21:30:39 -0700

    fix : hr/models.py

--- backend/hr/manager.py
+++ backend/hr/manager.py
@@ -1,5 +1,5 @@
 from django.db import models
 
-class ActiveEmployeeManager(models.manager):
+class ActiveEmployeeManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(is_deleted=False)
