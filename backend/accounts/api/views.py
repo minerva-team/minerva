e4e0aecaccf58eb0c202493675b8277f7ff6de63
shahinzam101 <shahinzam1402@gmail.com>
2026-06-24 18:57:57 +0330

    chore: add corsheaders to setting and ignore error in views.py accounts\api

--- backend/accounts/api/views.py
+++ backend/accounts/api/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
-from drf_spectacular.utils import extend_schema, OpenApiResponse
+from drf_spectacular.utils import extend_schema, OpenApiResponse # type: ignore
 
 from accounts.models import User, OTPVerification
 from .serializers import OTPRequestSerializer, OTPVerifySerializer, UserSerializer
--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -56,6 +56,7 @@ DJANGO_APPS = [
 THIRD_PARTY_APPS = [
     "rest_framework",
     "drf_spectacular",
+    "corsheaders",
 ]
 
 LOCAL_APPS = [
