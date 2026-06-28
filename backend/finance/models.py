8154b86c7e2c19899c05533c91d0d790eec4b0e9
Mahtab Saberi <saberimahtab2002@gmail.com>
2026-06-27 18:54:42 -0700

    feat: add Category model

--- backend/finance/models.py
+++ backend/finance/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    TYPE_CHOICES = [
+        ('Income', 'Income'),
+        ('Expense', 'Expense'),
+    ]    
+
+    name = models.CharField(max_length=100, unique=True)
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+
+
+    def __str__(self):
+        return f"{self.name} - {self.type}"
