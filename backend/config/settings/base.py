48c1f7a7baddde413cb56bc13c8aa70b2a646f6e
shahinzam101 <shahinzam1402@gmail.com>
2026-07-28 23:08:26 +0330

    feat: update TIME_ZONE setting to Asia/Tehran in base.py

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -133,7 +133,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # --- I18N -------------------------------------------------------------------
 
 LANGUAGE_CODE = "en-us"
-TIME_ZONE = "UTC"
+TIME_ZONE = 'Asia/Tehran'
 USE_I18N = True
 USE_TZ = True
 
